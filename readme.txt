60324de4f29e1375e4d96ebb31f35a81264a6c18
Paul Stoute <paul.stoute@gmail.com>
2018-01-29 18:58:40 -0800

    Added New Features
    Added customized login image and link.
    
    Added admin dashboard widget.

---
+++ readme.txt
@@ -0,0 +1,34 @@
+=== Plugin Name ===
+Contributors: stoute
+Tags: hotjar, heatmap, analytics, marketing, funnel
+Requires at least: 4.6
+Tested up to: 4.9.2
+Stable tag: 1.0.0
+Requires PHP: 5.6
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+This plugin was been developed for Stoute.co clients and adds little bits of extra code to help the Stoute.co Clients succeed!
+
+== Description ==
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/` directory, or install the plugin through the WordPress plugins screen directly.
+1. Activate the plugin through the 'Plugins' screen in WordPress
+1. Use the *Tools > Hotjar for WordPress* page to add your Hotjar SiteID for the tracking code.
+
+== Frequently Asked Questions ==
+
+= I'm not a Stoute.co client. Can I still use this plugin? =
+
+Yes, of course! You don't need to be a Stoute.co client to use this plugin.
+
+= I'm not a Stoute.co client. How do I become one? =
+
+You'll head over to our website: https://www.stoute.co/ and request to become a client. Then we'll say welcome aboard!
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial Plugin Upload
--- stoute-powered-site.php
+++ stoute-powered-site.php
@@ -15,9 +15,24 @@
  // Add the custom login logo
  function sws_site_logo() {
    echo '<style type="text/css">
-        h1 a {background-image: url('.plugins_url( 'images/login-logo.png', __FILE__ ).') !important; }
+        h1 a {background-image: url('.plugins_url( 'images/login-logo.png', __FILE__ ).') !important; width: 320px !important; height: 130px !important; background-size: auto !important;}
         </style>';
  }
  add_action('login_head', 'sws_site_logo');
+ 
+ // Add a custom link to the new logo image on the admin page.
+ function sws_site_loginlogo_url($url) {
+   return 'https://www.stoute.co';
+ }
+ add_filter( 'login_headerurl', 'sws_site_loginlogo_url' );
 
- // 
+ // Add the Support Ticket Widget to the Admin Dashboard
+ add_action('wp_dashboard_setup', 'sws_site_dashboard_widgets');
+ function sws_site_dashboard_widgets() {
+   global $wp_meta_boxes;
+   wp_add_dashboard_widget('sws_site_contact_widget', 'Website Support', 'sws_site_dashboard_contact');
+ }
+ function sws_site_dashboard_contact() {
+   // Widget Content Here
+   echo '<p>Welcome to your site! Need help? Contact your Stoute.co team <a href="mailto:support@stoute.co">here</a>. </p>';
+ }
